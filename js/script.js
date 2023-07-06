3c0719361c97ff6bcd3a6c52e576755203484c6f
sharath <sharath.surendran.m@gmail.com>
2023-07-07 03:35:40 +0530

    movie and tv api fetched

--- js/script.js
+++ js/script.js
@@ -3,6 +3,23 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+//fetch data from api
+async function fetchApiData(endpoint) {
+  const API_KEY = '11a4948741e9c8b773fa0ab01a719418';
+  const API_URL = 'https://api.themoviedb.org/3/';
+
+  showSpinner();
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+
+  const data = await response.json();
+  hideSpinner();
+  return data;
+}
+
+//display 20 popular movies
 async function displayPopularMovies() {
   const { results } = await fetchApiData('movie/popular');
   console.log(results);
@@ -30,18 +47,49 @@ async function displayPopularMovies() {
   });
 }
 
-//fetch data from api
-async function fetchApiData(endpoint) {
-  const API_KEY = '11a4948741e9c8b773fa0ab01a719418';
-  const API_URL = 'https://api.themoviedb.org/3/';
+//display 20 most popular tv shows
+async function displayPopularShows() {
+  const { results } = await fetchApiData('tv/popular');
+  console.log(results);
 
-  const response = await fetch(
-    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
-  );
+  results.forEach((show) => {
+    const div = document.createElement('div');
+    // Logical Assignment (||=) This will assigns the right side value only if the left is a falsy value.
+    let img = `https://image.tmdb.org/t/p/w500${show.poster_path}`;
+    img ||= 'images/no-image.jpg';
+    div.classList.add('card');
+    div.innerHTML = `
+    <a href="tv-details.html?id=1">
+      <img src=${img} class="card-img-top" alt=${show.name} />
+    </a>
+    <div class="card-body">
+      <h5 class="card-title">${show.name}</h5>
+      <p class="card-text">
+        <small class="text-muted">Aired: ${show.first_air_date}</small>
+      </p>
+    </div>
+   
+      `;
+    document.querySelector('#popular-shows').appendChild(div);
+  });
+}
 
-  const data = await response.json();
+//spinner
+function showSpinner() {
+  document.querySelector('.spinner').classList.add('show');
+}
+function hideSpinner() {
+  document.querySelector('.spinner').classList.remove('show');
+}
 
-  return data;
+//highlight active links
+function highlightActiveLinks() {
+  const navLinks = document.querySelectorAll('.nav-link');
+  navLinks.forEach((link) => {
+    if (link.getAttribute('href') === global.currentPage) {
+      link.classList.add('active');
+    }
+  });
 }
 
 //init app
@@ -55,6 +103,7 @@ function init() {
     }
     case '/shows.html': {
       console.log('Shows');
+      displayPopularShows();
       break;
     }
     case '/movie-details.html': {
@@ -71,16 +120,6 @@ function init() {
     }
   }
   highlightActiveLinks();
-}
-
-//highlight active links
-function highlightActiveLinks() {
-  const navLinks = document.querySelectorAll('.nav-link');
-  navLinks.forEach((link) => {
-    if (link.getAttribute('href') === global.currentPage) {
-      link.classList.add('active');
-    }
-  });
 }
 
 document.addEventListener('DOMContentLoaded', init);

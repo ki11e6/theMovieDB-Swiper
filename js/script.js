354e397c56e3d72372a82e084fc3b9f7ae66d026
sharath <sharath.surendran.m@gmail.com>
2023-07-07 18:45:51 +0530

    slider using swiperjs

--- js/script.js
+++ js/script.js
@@ -19,11 +19,59 @@ async function fetchApiData(endpoint) {
   return data;
 }
 
+//Display slider movies
+async function displaySlider() {
+  const { results } = await fetchApiData('movie/now_playing');
+  console.log(results);
+  results.forEach((movie) => {
+    let img = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    img ||= 'images/no-image.jpg';
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide');
+    div.innerHTML = `
+          <a href="movie-details.html?id=${movie.id}">
+            <img src=${img} alt=${movie.original_title} />
+          </a>
+          <h4 class="swiper-rating">
+            <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(
+              1
+            )} / 10
+          </h4>
+    `;
+    document.querySelector('.swiper-wrapper').appendChild(div);
+    initSwiper();
+  });
+}
+
+function initSwiper() {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  });
+}
+
 //display 20 popular movies
 async function displayPopularMovies() {
   try {
     const { results } = await fetchApiData('movie/popular');
-    console.log(results);
+    // console.log(results);
 
     results.forEach((movie) => {
       const div = document.createElement('div');
@@ -261,6 +309,7 @@ function init() {
     case '/':
     case '/index.html': {
       console.log('Home');
+      displaySlider();
       displayPopularMovies();
       break;
     }

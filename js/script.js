65b1401a5b0b46633950781947aae9635e1e53fb
sharath <sharath.surendran.m@gmail.com>
2023-07-07 10:34:54 +0530

    map vs forEach,windows.location.search,join,movie-details

--- js/script.js
+++ js/script.js
@@ -21,35 +21,39 @@ async function fetchApiData(endpoint) {
 
 //display 20 popular movies
 async function displayPopularMovies() {
-  const { results } = await fetchApiData('movie/popular');
-  console.log(results);
+  try {
+    const { results } = await fetchApiData('movie/popular');
+    console.log(results);
 
-  results.forEach((movie) => {
-    const div = document.createElement('div');
-    // Logical Assignment (||=) This will assigns the right side value only if the left is a falsy value.
-    let img = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-    img ||= 'images/no-image.jpg';
-    div.classList.add('card');
-    div.innerHTML = `
-    
-    <a href="movie-details.html?id=${movie.id}">
-      <img src=${img} class="card-img-top" alt=${movie.title} />
-    </a>
-    <div class="card-body">
-      <h5 class="card-title">${movie.title}</h5>
-      <p class="card-text">
-        <small class="text-muted">Release:${movie.release_date}</small>
-      </p>
-    </div>
- 
-    `;
-    document.querySelector('#popular-movies').appendChild(div);
-  });
+    results.forEach((movie) => {
+      const div = document.createElement('div');
+      // Logical Assignment (||=) This will assigns the right side value only if the left is a falsy value.
+      let img = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+      img ||= 'images/no-image.jpg';
+      div.classList.add('card');
+      div.innerHTML = `
+      
+      <a href="movie-details.html?id=${movie.id}">
+        <img src=${img} class="card-img-top" alt=${movie.title} />
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">${movie.title}</h5>
+        <p class="card-text">
+          <small class="text-muted">Release:${movie.release_date}</small>
+        </p>
+      </div>
+   
+      `;
+      document.querySelector('#popular-movies').appendChild(div);
+    });
+  } catch (e) {
+    console.log('Error:', e.message);
+  }
 }
 
 //display 20 most popular tv shows
 async function displayPopularShows() {
-  const { results } = await fetchApiData('tv/popular');
+  const { results } = await fetchApiData('tv/top_rated');
   console.log(results);
 
   results.forEach((show) => {
@@ -72,6 +76,68 @@ async function displayPopularShows() {
       `;
     document.querySelector('#popular-shows').appendChild(div);
   });
+}
+
+//movie details
+async function getMovieDetails() {
+  try {
+    const movieId = window.location.search.split('=')[1];
+    const movie = await fetchApiData(`movie/${movieId}`);
+    console.log(movie);
+    const div = document.querySelector('#movie-details');
+    let img = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    img ||= 'images/no-image.jpg';
+    div.innerHTML = `
+    <div class="details-top">
+        <div>
+          <img src=${img} class="card-img-top" alt=${movie.title} />
+        </div>
+        <div>
+          <h2>${movie.title} </h2>
+          <p>
+            <i class="fas fa-star text-primary"></i>
+            ${movie.vote_average.toPrecision(2)} / 10
+          </p>
+          <p class="text-muted">Release Date: ${movie.release_date}</p>
+          <p>
+            ${movie.overview}
+          </p>
+          <h5>Genres</h5>
+          <ul class="list-group">
+          ${movie.genres.map((g) => `<li>${g.name}</li>`).join('')}
+          </ul>
+          <a href=${
+            movie.homepage
+          } target="_blank" class="btn">Visit Movie Homepage</a>
+        </div>
+      </div>
+      <div class="details-bottom">
+        <h2>Movie Info</h2>
+        <ul>
+          <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(
+            movie.budget
+          )}</li>
+          <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
+            movie.revenue
+          )}</li>
+          <li><span class="text-secondary">Runtime:</span> ${
+            movie.runtime
+          } minutes</li>
+          <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+        </ul>
+        <h4>Production Companies</h4>
+        <div class="list-group">${movie.production_companies
+          .map((c) => `<span>${c.name}</span>`)
+          .join(', ')}</div>
+      </div>
+    `;
+  } catch (e) {
+    console.log(e);
+  }
+}
+//add commas
+function addCommasToNumber(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
 //spinner
@@ -107,6 +173,7 @@ function init() {
       break;
     }
     case '/movie-details.html': {
+      getMovieDetails();
       console.log('Movie Details');
       break;
     }
